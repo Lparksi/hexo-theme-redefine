119df070cceb4d8367d93ae10062e04e5c6633ba
Lparksi <l.moluan@outlook.com>
2026-01-25 12:38:35 +0800

    fix(pangu): disable global auto spacing to prevent unwanted spaces in footer

--- source/js/build/plugins/pangu.js
+++ source/js/build/plugins/pangu.js
@@ -1,2 +1,2 @@
-function initPanguJS(){pangu.spacingElementByClassName("markdown-body"),pangu.autoSpacingPage()}document.addEventListener("DOMContentLoaded",initPanguJS);try{swup.hooks.on("page:view",initPanguJS)}catch(n){}
+function initPanguJS() { pangu.spacingElementByClassName("markdown-body") } document.addEventListener("DOMContentLoaded", initPanguJS); try { swup.hooks.on("page:view", initPanguJS) } catch (n) { }
 //# sourceMappingURL=pangu.js.map
--- source/js/plugins/pangu.js
+++ source/js/plugins/pangu.js
@@ -2,11 +2,11 @@ function initPanguJS() {
   // Add space between Chinese and English
   pangu.spacingElementByClassName("markdown-body");
 
-  pangu.autoSpacingPage();
+  // pangu.autoSpacingPage();
 }
 
 document.addEventListener("DOMContentLoaded", initPanguJS);
 
 try {
   swup.hooks.on("page:view", initPanguJS);
-} catch (e) {}
+} catch (e) { }
